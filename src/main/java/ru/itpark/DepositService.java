eb5607942becf418f529ee81d49da3f5a6636f8e
Aleksander <rit>
2018-12-06 16:54:44 +0300

    Create test

---
+++ src/main/java/ru/itpark/DepositService.java
@@ -0,0 +1,8 @@
+package ru.itpark;
+
+public class DepositService {
+    public int calculateDeposit(int deposit, int percent, int date) {
+        int sum = (deposit * percent * date / 100) + deposit;
+        return sum;
+    }
+}
---
+++ src/test/java/ru/itpark/DepositServiceTest.java
@@ -0,0 +1,17 @@
+package ru.itpark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepositServiceTest {
+
+    @Test
+    void calculateDeposit() {
+        DepositService depositService = new DepositService();
+        int deposit = depositService.calculateDeposit(1_000_000, 10, 3);
+
+        assertEquals(1_300_000,deposit);
+
+    }
+}
